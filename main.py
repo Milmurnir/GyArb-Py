4b1013adb0b7ec8cb8eaf7c01f1aff5ead20d8a6
Milmurnir <te17niceva@falufri.se>
2019-10-02 18:33:23 +0200

    Idk

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import pygame
 import random
 import keyboard
+from classes import *
 from win32api import * 
 
 
-display = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+display = pygame.display.set_mode((1920,1080))
 resolution = (GetSystemMetrics(0),GetSystemMetrics(1))
 playing = True
+tileSize = 120
+tileList = []
 
+def makeTerrain():
+    for i in range(14):
+        yCord = i * tileSize
+        for o in range(8):
+            xCord = o * tileSize
 
-def makeTerrain()
+        tile = Tile((xCord,yCord),tileSize)
+        tileList.append(tile)
 
+def start():
+    pygame.init()
+    makeTerrain()
 
+def update():
+    for tile in tileList:
+        tile.updateTile()
 
-
-
-
+start()
 
 while playing:
     for event in pygame.event.get():
@@ -25,3 +38,6 @@ while playing:
 
         if keyboard.is_pressed("esc"):
             playing = False
+    
+    update()
+    pygame.display.update()
